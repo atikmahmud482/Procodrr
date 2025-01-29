31212122dcf62c1d30184518529e700abbbb73a2
atikmahmud482 <atikmahmud482@gmail.com>
2025-01-29 23:40:21 +0600

    video-9

--- first folder/script.js
+++ first folder/script.js
@@ -1,16 +1,25 @@
-let firstName = "atik";
-let lastName = "mahmud";
-let age = 24;
-const yearOfBirth = 2000;
-console.log(firstName);
+/* Video 6,7,8 */
+// let firstName = "atik";
+// let lastName = "mahmud";
+// let age = 24;
+// const yearOfBirth = 2000;
+// console.log(firstName);
 
-let userInfo =
-  "Hi, my name is " +
-  firstName +
-  " " +
-  lastName +
-  ", my age is " +
-  age +
-  ". i was born in " +
-  yearOfBirth;
-console.log(userInfo);
+// let userInfo =
+//   "Hi, my name is " +
+//   firstName +
+//   " " +
+//   lastName +
+//   ", my age is " +
+//   age +
+//   ". i was born in " +
+//   yearOfBirth;
+// console.log(userInfo);
+
+/* VIDEO 9 */
+
+// let isConfirm = confirm("your massage goes here...");
+
+// const alertResult = alert("Are you sure?")
+
+// const userInput = prompt("Please enter your name.");

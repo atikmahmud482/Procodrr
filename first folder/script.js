a9fb7d6b499810209a6c3740d770876fc82e2533
atikmahmud482 <atikmahmud482@gmail.com>
2025-01-29 22:54:00 +0600

    mid

--- first folder/script.js
+++ first folder/script.js
@@ -1,2 +1,8 @@
-let userName = "atik";
-console.log(userName);
+let firstName = "atik";
+let lastName = "mahmud";
+let age = 24;
+const yearOfBirth = 2000;
+console.log(firstName);
+
+let userInfo = "Hi, my name is " + firstName + " " + lastName;
+console.log(userInfo);

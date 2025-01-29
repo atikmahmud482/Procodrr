7532967006013ff9185b9dc5cae3802a6407f95c
atikmahmud482 <atikmahmud482@gmail.com>
2025-01-29 23:25:33 +0600

    video-8

--- first folder/script.js
+++ first folder/script.js
@@ -4,5 +4,13 @@ let age = 24;
 const yearOfBirth = 2000;
 console.log(firstName);
 
-let userInfo = "Hi, my name is " + firstName + " " + lastName;
+let userInfo =
+  "Hi, my name is " +
+  firstName +
+  " " +
+  lastName +
+  ", my age is " +
+  age +
+  ". i was born in " +
+  yearOfBirth;
 console.log(userInfo);

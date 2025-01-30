4372802bbbc5065fc63e87c6323d56f4ec01dab3
atikmahmud482 <atikmahmud482@gmail.com>
2025-01-31 02:26:27 +0600

    video 14

--- first folder/script.js
+++ first folder/script.js
@@ -35,6 +35,7 @@ console.log(trimm.trim().toUpperCase());
 const replaceMassage = massage.replace("h", "H");
 console.log(replaceMassage);
  */
+
 /* Video 11 and 12 
 console.log(Boolean("")); //false
 console.log(Boolean(undefined)); //false
@@ -42,8 +43,29 @@ console.log(Boolean(null)); //false
 console.log(Boolean(-0)); //falsy
 console.log(Boolean(!0)); //true
 */
+
 /* Video 13 
 const number1 = 23;
 const number2 = 34;
 console.log(number1 > number1);
+*/
+
+/* Video 14 
+// const userAge = 24;
+// console.log(userAge <= 18);
+// console.log(userAge >= 18 && userAge <= 24);
+// console.log(0 || 2);
+// console.log(0 && 2);
+// console.log("" && "hello");
+const andResult2 = "" && "Hello";
+const orResult2 = "" || "Hello";
+
+const andResult3 = "Hello" && null;
+const orResult3 = "Hello" || null;
+
+const andResult4 = "Hello" && -4 + 4;
+const orResult4 = undefined || 4 + 8 * 5;
+
+const andResult5 = "Hello" && console.log("hello1");
+const orResult5 = undefined || console.log("hello2");
 */

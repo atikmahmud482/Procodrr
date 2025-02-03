2360ee11d02e8670e4423570beee6a2f815e6638
atikmahmud482 <atikmahmud482@gmail.com>
2025-02-03 17:12:53 +0600

    video 18 start

--- first folder/script.js
+++ first folder/script.js
@@ -106,3 +106,22 @@ const orResult5 = undefined || console.log("hello2");
 
 */
 /* Video 18 */
+const dayNumber = 3;
+
+if (dayNumber === 0) {
+  console.log("It is Sunday Today");
+} else if (dayNumber === 1) {
+  console.log("It is Monday Today");
+} else if (dayNumber === 2) {
+  console.log("It is Tuesday Today");
+} else if (dayNumber === 3) {
+  console.log("It is Wednesday Today");
+} else if (dayNumber === 4) {
+  console.log("It is Thursday Today");
+} else if (dayNumber === 5) {
+  console.log("It is Friday Today");
+} else if (dayNumber === 6) {
+  console.log("It is Saturday Today");
+} else {
+  console.log("Please Enter a Valid Day Number");
+}

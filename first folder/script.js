512e0bf60de3e04d5d18b74955cd3be46c674cd4
atikmahmud482 <atikmahmud482@gmail.com>
2025-02-08 21:42:46 +0600

    shift , unshift add

--- first folder/script.js
+++ first folder/script.js
@@ -234,3 +234,9 @@ console.log(fruits);
 
 const rainBow = ["red", "green"];
 */
+/* Video 24 */
+const evenNumber = [2, 4, 6, 8, 10];
+// console.log(evenNumber.shift());
+// console.log(evenNumber.unshift());
+const animals = ["Dog", "cat", "cow"];
+const oddNumber = [1, 3, 5, 7, 9];
